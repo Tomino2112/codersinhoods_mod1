0583fd2ccc03a72c26c28484c0c51dfb8cbdb5fd
atikP <atikpatel@hotmail.co.uk>
2021-02-01 11:05:05 +0000

    exercise 2 passed

--- 02-js-types-exercise/index.js
+++ 02-js-types-exercise/index.js
@@ -3,19 +3,21 @@
  *
  * create a variable {greeting} which should be a string
  */
-
+const greeting = "Hello";
 /**
  * Exercise 2
  *
  * create a variable {name} which should be a string
  */
-
+const name = "Reader";
 /**
  * Exercise 3
  *
  * create a variable {phrase} which should be a
  * concatenation of {greeting} and {name}
  */
+let phrase = `${greeting}, ${name} how are you today? `;
+
 
 /**
  * Exercise 4
@@ -23,33 +25,37 @@
  * create a variable {price} which should has type number,
  * it should be a float.
  */
-
+const price = 4.99;
 /**
  * Exercise 5
  *
  * create a variable {quantity} which should has type number.
  */
-
+const quantity = 2;
 /**
  * Exercise 6
  *
  * create a variable {total} which should be
  * equal to price multiplied by quantity
  */
-
+const total = price * quantity;
+// console.log(total);
 /**
  * Exercise 7
  *
  * create a variables {age1}, {age2}, {age3} which should be
  * integers
  */
-
+const age1 =11;
+const age2 = 12;
+const age3 = 13;
 /**
  * Exercise 8
  *
  * create a variable {averageAge} which should be
  * the mean of age1, age2 and age3
  */
+let averageAge = (age1 + age2 + age3)/3;
 
 /**
  * Exercise 9
@@ -57,21 +63,21 @@
  * create a variable {loggedIn} which should be
  * a boolean and equals true.
  */
-
+const loggedIn = true;
 /**
  * Exercise 10
  *
  * create a variable {adminUser} which should be
  * a boolean and equals false.
  */
-
+let adminUser = false;
 /**
  * Exercise 11
  *
  * create a variable {missingInfo} which should be
  * equal to undefined
  */
-
+let missingInfo = undefined;
 /**
  * Exercise 12
  *
