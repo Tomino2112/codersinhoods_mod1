e2fdc5dbbc8340d2e3b72cfcdebebe9d55254c5f
atikP <atikpatel@hotmail.co.uk>
2021-02-15 14:28:34 +0000

    Exercise 21 passed

--- 21-adding-properties-to-objects-exercise/index.js
+++ 21-adding-properties-to-objects-exercise/index.js
@@ -8,17 +8,25 @@ const user = {};
  *
  * add property "firstName" to "user" and assign any name
  */
-
+user.firstName = "Atik";
+console.log(user);
 /**
  * Exercise 2
  *
  * add property "lastName" to "user" and assign any last name
  */
-
+user.lastName = "Patel";
+console.log(user);
 /**
  * Exercise 3
  * create a function "addANewProperty" which takes
  * 2 params, first - property name and second - property value
  * and set a new property for 'user' object
  */
+function addANewProperty(propertyName, PropertyValue) {
+  return user[propertyName] = PropertyValue;
+}
 
+console.log(addANewProperty('london', 12));
+
+console.log(user);
