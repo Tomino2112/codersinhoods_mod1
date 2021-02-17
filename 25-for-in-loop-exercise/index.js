9317a48833888488ceb5c1f7bfd3b782be532a39
atikP <atikpatel@hotmail.co.uk>
2021-02-17 15:43:01 +0000

    Exercise 25 passed

--- 25-for-in-loop-exercise/index.js
+++ 25-for-in-loop-exercise/index.js
@@ -4,18 +4,38 @@
  * take object as a param and console log each property in a format
  * "Property name: {propertyName}"
  */
-
+function logProperties(properties) {
+  for (let property in properties) {
+    console.log("Property name: " +property);
+  }
+}
 /**
  * Exercise 2
  * Create a function {getAllValues} that takes an object
  * and returns an array of all values.
  */
 
+function getAllValues(object) {
+  let values= []
+  for (let value in object) {
+   values.push(object[value]);
+  }
+  return values;
+}
+
 /**
  * Exercise 3
  * Create a function {getAllValuesAsString} that takes an object
  * and returns all values as one string with space separation.
  */
+function getAllValuesAsString(object) {
+  let string = "";
+  for (let word in object) {
+    string = string+object[word]+' ';
+
+  }
+  return string.trim();
+}
 
 /**
  * Exercise 4
@@ -23,3 +43,12 @@
  * and log property and values as string in a next format:
  * "{key}: {value}"
  */
+
+ function propertyValueString(object) {
+  let newObj = "";
+  for (let keyValue in object) {
+    newObj = keyValue +":"+' '+ object[keyValue];
+    console.log(newObj);
+  }
+  
+ }
