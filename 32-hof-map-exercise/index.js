a87f3ac58912c2f4fae55f45e8793409880c4f3a
atikP <atikpatel@hotmail.co.uk>
2021-02-22 15:10:31 +0000

    Exercise 32 passed

--- 32-hof-map-exercise/index.js
+++ 32-hof-map-exercise/index.js
@@ -9,7 +9,20 @@
  * array of numbers, convert all negatives to positives and return
  * a new array.
  */
+const convertToPositive = (negNumbers) => {
+  const posNumbers = negNumbers.map(negNumber => {
+  const posNumbers =[];
+  if(negNumber <0) {
+    // console.log(negNumber);
+    return negNumber*(-1);
+  } else {
+    return negNumber
+  }
+})
+return posNumbers;
+}
 
+console.log(convertToPositive([-1,-2,-3,-5]))
 /**
  * Exercise 2
  *
@@ -22,14 +35,24 @@
  *   {name: 'John', age: 25}
  * ]) will return ['Alex', 'John']
  */
-
+const getAllNames = users => {
+  const userArray = users.map(user => {
+    return user.name;
+  });
+  return userArray;
+}
 /**
  * Exercise 3
  *
  * create a function {greetAll} that takes array of names as a parameter
  * and return a new array of greetings "Hi, {name}!"
  */
+const greetAll = names => {
+  const newNames = names.map(name => `Hi, ${name}!`) ;
+  return newNames
+}
 
+console.log(greetAll(["harry","john"]))
 /**
  * Exercise 4
  *
@@ -37,3 +60,14 @@
  * and multiply all numbers inside array by 5 and return the whole array
  * with multiplied numbers
  */
+const multiplyAllNumbers = numbers => {
+  const multipliedArray = numbers.map(number => {
+    if(typeof number == "number")
+    { return number*5;
+    } else {
+      return number;
+    }
+  })
+  return multipliedArray;
+}
+console.log(multiplyAllNumbers([1,"hello",2,3,4]));
