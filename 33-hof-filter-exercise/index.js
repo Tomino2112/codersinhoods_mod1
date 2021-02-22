c4da3bfb79195108fc712f54f7c53d7340663251
atikP <atikpatel@hotmail.co.uk>
2021-02-22 16:39:00 +0000

    Exercise 33 passed

--- 33-hof-filter-exercise/index.js
+++ 33-hof-filter-exercise/index.js
@@ -4,7 +4,7 @@
  * create a function {noStrings} that takes an array
  * as a parameter and return an array but without strings
  */
-
+const noStrings = list => list.filter(listItem => (typeof listItem!="string"));
 /**
  * Exercise 2
  *
@@ -12,15 +12,16 @@
  * as a parameter and return an array of strings by removing
  * every other type
  */
-
+const justStrings = list => list.filter(listItem => (typeof listItem == "string"));
 /**
  * Exercise 3
  *
  * create a function {justStringsLongerThan5} that takes
- * an array as a parameter and return an array of strings
+ * an array as a parameter and retur
+ * n an array of strings
  * that are longer than 5 characters
  */
-
+const justStringsLongerThan5 = list => list.filter(listItem => ((typeof listItem=="string") && (listItem.length > 5)));
 /**
  * Exercise 4
  *
@@ -28,14 +29,14 @@
  * an array as a parameter and return an array of numbers
  * that are more than 13
  */
-
+const numbersMoreThan13 =list => list.filter(listItem => listItem > 13);
 /**
  * Exercise 5
  *
  * create a function {objectsOnly} that takes
  * an array as a parameter and return an array of objects
  */
-
+const objectsOnly = objects => objects.filter(object => (typeof object == "object" ));
 /**
  * Exercise 6
  *
@@ -43,3 +44,18 @@
  * an array as a parameter and return an array of truthy values.
  * IMPORTANT: You still need to keep number 0.
  */
+const truthyOnly = values => values.filter(value => ( Boolean(value) ||value=== 0 ));
+
+const param1 = [
+  "kitten",
+  {},
+  0,
+  { name: "Alex" },
+  undefined,
+  NaN,
+  ["pet"],
+  "",
+  26,
+  false
+];
+console.log(truthyOnly(param1));
