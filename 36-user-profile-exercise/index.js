dfa384445092c7c7720a94eb247c81570e8fb2e7
atikP <atikpatel@hotmail.co.uk>
2021-02-24 17:08:08 +0000

    Exercise 36 passed

--- 36-user-profile-exercise/index.js
+++ 36-user-profile-exercise/index.js
@@ -1,18 +1,19 @@
 // install and  import "readline-sync" npm package before you do exercises
-
+const readlineSync = require('readline-sync');
+const chalk = require('chalk');
 const user = {};
 /**
  * Exercise 1
  *
  * ask user for a first name and save response to {user.firstName}
  */
-
+ user.firstName = chalk.bgBlue.yellow(readlineSync.question("May i have your FIRST name?"))
 /**
  * Exercise 2
  *
  * ask user for a last name and save response to {user.lastName}
  */
-
+user.lastName = chalk.bgBlue.yellow(readlineSync.question("May i have your LAST name?"));
 console.log(`Hello ${user.firstName} ${user.lastName}!`);
 
 /*
@@ -20,13 +21,13 @@ console.log(`Hello ${user.firstName} ${user.lastName}!`);
  *
  * ask user for a age and save response to {user.age}
  */
-
+user.age = chalk.blue(readlineSync.question("May i have your age?"))
 /*
  * Exercise 4
  *
  * ask user for a email and save response to {user.email}
  */
-
+user.email = chalk.red(readlineSync.question("May i have your emai address?"));
 /**
  * OPTIONAL:
  *
