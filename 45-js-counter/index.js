ca47c39f2311dc364f8ad0eb459eb498f6f64dd6
atikP <atikpatel@hotmail.co.uk>
2021-03-06 22:08:54 +0000

    Exercise 45 partPassed

--- 45-js-counter/index.js
+++ 45-js-counter/index.js
@@ -10,7 +10,23 @@ let counter = 0;
  *
  * NOTE: ".counter_value" should always represent the current value of {counter}
  */
+const action = (symbol) => {
+  const counterVal = document.querySelector('.counter_value')
 
+  if (symbol == '+') {
+    counter = (counter + step);
+    counterVal.innerText = parseInt(counter);
+    return counter;
+
+  } else if (symbol == '-') {
+    counter = (counter - step);
+    counterVal.innerText = parseInt(counter);
+    return counter;
+  }
+
+
+  
+}
 /**
  * Exercise 2
  *
@@ -22,7 +38,16 @@ let counter = 0;
  * NOTE: {step} should be an integer
  *
  */
+const setStepButton = document.querySelector('.step_form');
+let stepInput = document.querySelector('.step_form input');
 
+setStepButton.addEventListener("submit",(event) => {
+  event.preventDefault();
+  step = parseInt(stepInput.value);
+  document.querySelector('.inc .step_value').innerText = step;
+  stepInput.value = 1;
+
+})
 /**
  * Exercise 3
  *
@@ -31,7 +56,11 @@ let counter = 0;
  *
  * NOTE: remember to use your {action} function
  */
+const decBtn = document.querySelector('#decrement');
 
+decBtn.addEventListener("click", ()=> {
+  action("-");
+})
 /**
  * Exercise 4
  *
@@ -40,7 +69,11 @@ let counter = 0;
  *
  * NOTE: remember to use your {action} function
  */
+ const incBtn = document.querySelector('#increment');
 
+ incBtn.addEventListener("click", ()=> {
+   action("+");
+ })
 /**
  * Exercise 5
  *
@@ -49,7 +82,17 @@ let counter = 0;
  *
  * NOTE: ".counter_value" should represent current state of counter
  */
+ let interlID;
 
+const autoDec = document.querySelector("#auto_decrement");
+
+autoDec.addEventListener("click", ()=> {
+  interID =
+  setInterval(() => {
+    action("-");
+    console.log(typeof counter + counter);
+  }, 1000);
+})
 /**
  * Exercise 6
  *
@@ -58,10 +101,22 @@ let counter = 0;
  *
  * NOTE: ".counter_value" should represent current state of counter
  */
-
+ const autoInc = document.querySelector("#auto_increment");
+ autoInc.addEventListener("click", ()=> {
+    interID =
+   setInterval(() => {
+     action("+");
+     console.log(typeof counter + counter);
+   }, 1000);
+ })
 /**
  * Exercise 7
  *
  * when the user clicks on "#stop_auto",
  * the auto counter should stop
  */
+const stopBtn = document.querySelector('#stop_auto');
+stopBtn.addEventListener("click",() => {
+  clearInterval(interID);
+  
+})
