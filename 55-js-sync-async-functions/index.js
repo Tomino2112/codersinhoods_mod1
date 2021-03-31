e0bd6910353821668cbae7cc2bb1102626fb445f
atikP <atikpatel@hotmail.co.uk>
2021-03-31 14:58:39 +0100

    Exercise 55 re-passed

--- 55-js-sync-async-functions/index.js
+++ 55-js-sync-async-functions/index.js
@@ -19,28 +19,28 @@ const syncCookIngredient = ({ name, time }) => {
  *
  * create a function {syncCookMeal} which will recieve an
  * array of {ingredientsToCook}, (similar to the array
- * defined on line 1) and call {syncCookIngredient} 
+ * defined on line 1) and call {syncCookIngredient}
  * for each ingredient in the array
- * 
+ *
  * After all the ingredients are cooked,
  * log the message "Soup is ready to serve"
  */
 const syncCookMeal = (ingredientArray) => {
-  ingredientArray.forEach(ingredient => {
+  ingredientArray.forEach((ingredient) => {
     syncCookIngredient(ingredient);
-  })
-  console.log("Soup is ready to serve")
-}
+  });
+  console.log("Soup is ready to serve");
+};
 // syncCookMeal(ingredients);
 
 /**
  * Exercise 2
  *
  * create a function {asyncCookIngredient} which takes 1 argument:
- * 
+ *
  * an {ingredient} object (like the objects in the array on line 1)
  * with 2 keys: name and time.
- * 
+ *
  * Log out the message "Start cooking INGREDIENT_NAME"
  * then after the cooking time has elapsed,
  * log out "INGREDIENT_NAME cooked!". Both times displaying
@@ -48,12 +48,15 @@ const syncCookMeal = (ingredientArray) => {
  *
  */
 
-const asyncCookIngredient = ({name, time}) =>{
-    console.log("Start cooking "+ name)
+const asyncCookIngredient = ({ name, time }) => {
+  console.log("Start cooking " + name);
+  return new Promise((resolve) => {
     setTimeout(() => {
-      console.log(name + ' cooked!');
-    }, (time));
-}
+      console.log(name + " cooked!");
+      resolve();
+    }, time);
+  });
+};
 // const ingredientsForTests = [
 //   { name: "carrot", time: 1000 },
 //   { name: "rice", time: 2000 },
@@ -65,18 +68,18 @@ const asyncCookIngredient = ({name, time}) =>{
  * create a function {asyncCookMeal} which will which will recieve an
  * array of {ingredientsToCook} and call {asyncCookIngredient} for
  * eahc ingredient
- * 
+ *
  * After all the ingredients are cooked,
  * log the message "Soup is ready to serve"
  */
 
-
-
 const asyncCookMeal = async (ingredientArray) => {
-  await ingredientArray.forEach(async ingredient =>{
-  await asyncCookIngredient(ingredient);
-})
-console.log( "Soup is ready to serve");
-}
+  const promises = await ingredientArray.map(async (ingredient) => {
+    return await asyncCookIngredient(ingredient);
+  });
+ 
+  await Promise.all(promises); 
+  console.log("Soup is ready to serve");
+};
 
-// asyncCookMeal(ingredients);
+asyncCookMeal(ingredients);
